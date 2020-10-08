e4d67c73e38b51235ef1dde2b6c4ade4bec9d224
JD <james@baked.digital>
2020-10-08 14:28:51 +0100

    init :tada:

---
+++ src/components/NotFoundComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="notfound">
+    <h2>HELLO, IS IT ME YOUR LOOKING FOR?</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFoundComponent'
+}
+</script>
+
+<style>
+</style>
