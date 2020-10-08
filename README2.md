126566e9128aed1a5e60d2e04d48a7b097c591fb
JD <james@baked.digital>
2020-10-08 16:33:05 +0100

    vuetify & storybook

---
+++ README2.md
@@ -0,0 +1,16 @@
+# VUE STORYBOOK LIBRARY
+
+storybook
+vuetify material ui
+https://vuetifyjs.com/en/
+
+## Build Storybook as a static web application
+
+https://storybook.js.org/docs/vue/workflows/publish-storybook
+
+build storybook: `yarn build-storybook`
+serve locally: `npx http-server storybook-static`
+
+## Publish Storybook online
+
+https://storybook.js.org/docs/vue/workflows/publish-storybook#publish-storybook-online
--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,5 @@
-// https://cli.vuejs.org/config/
-// import global sass files
-// https://css-tricks.com/how-to-import-a-sass-file-into-every-vue-component-in-an-app/
-// module.exports = {
-//   css: {
-//     loaderOptions: {
-//       scss: {
-//         prependData: `@import "~@/scss/main.scss";`
-//       }
-//     }
-//   },
-//   devServer: {
-//     proxy: {
-//       '^/webhook': {
-//         target: 'https://hooks.zapier.com/hooks/catch/519363/o8az5jw/',
-//         ws: true,
-//         changeOrigin: true,
-//         pathRewrite: { '^/webhook': '' },
-//       }
-//     }
-//   }
-// };
+module.exports = {
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
