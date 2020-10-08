835648bd84b97508cf2ea4d235be9ecbdd33379d
JD <james@baked.digital>
2020-10-09 00:55:37 +0100

    with fontawesome icons loaded in css

--- src/stories/organisms/Footer.stories.js
+++ src/stories/organisms/Footer.stories.js
@@ -1,5 +1,7 @@
 import Footer from './Footer.vue'
+// import logo from static files
 import logoFile from '../assets/flow.svg'
+
 export default {
   title: 'organisms/Footer',
   component: Footer,
@@ -10,25 +12,34 @@ export default {
 };
 
 // We create a “template” of how args map to rendering
-
-// const Template = (args) => <Footer {...args} />;
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { Footer },
-  template: '<Footer :name="name" :companyNo="companyNo" :privacy="privacy" :logoSrc="Logo.src"/>',
+  template: '<Footer :name="name" :companyNo="companyNo" :privacy="privacy" :logoSrc="logoSrc" :address="address"/>',
 });
 
+// create an image object for the logo
 const Logo = {
   src: logoFile,
   alt: 'logo'
 }
+// const facebook = icon({ prefix: 'fab', iconName: 'facebookSquare' })
+// const linkedin = icon({ prefix: 'fab', iconName: 'linkedin' })
+// export const withAnImage = () => (
+//   <img src={image.src} alt={image.alt} />
+// );
 // Each story then reuses that template
 export const Primary = Template.bind({});
 
 Primary.args = {
   primary: true,
   name: 'Company Name',
+  address: [
+    '349 Manchester Road',
+    'Manchester',
+    'M12 ABC'
+  ],
   companyNo: '12345678',
   privacy: '/privacy.pdf',
-  logo: Logo
+  logoSrc: Logo.src
 };
--- src/stories/organisms/Footer.vue
+++ src/stories/organisms/Footer.vue
@@ -4,30 +4,46 @@
       <div class="footer__grid">
         <div class="footer__grid__item brand">
           <div class="brand__logo">
-            <img :src="logoSrc" draggable="false">
+            <img :src="logoSrc" draggable="false" />
           </div>
           <div class="brand__name">
-            <h3 class="brand__name__1">
-              VERTEX
-            </h3>
+            <!-- <h3 class="brand__name__1">VERTEX</h3> -->
           </div>
         </div>
         <div class="footer__grid__item social">
           <h3>SOCIAL</h3>
           <ul>
-            <li><a href="https://www.linkedin.com/company/vertex-scaffolding-solutions-ltd/" aria-label="Find us on LinkedIn"><i class="fab fa-linkedin" /></a></li>
-            <li><a href="https://www.facebook.com/VERTEXScaffoldingSolutions/" aria-label="Find us on Facebook"><i class="fab fa-facebook-square" /></a></li>
+            <li>
+              <a
+                href="https://www.linkedin.com/company/vertex-scaffolding-solutions-ltd/"
+                aria-label="Find us on LinkedIn"
+                ><i class="fab fa-linkedin" /></a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/VERTEXScaffoldingSolutions/"
+                aria-label="Find us on Facebook"
+                ><i class="fab fa-facebook-square" />
+              </a>
+            </li>
             <!-- <li><a href="#" aria-label="Find us on Twitter"><i class="fab fa-twitter-square" /></a></li> -->
           </ul>
         </div>
         <div class="footer__grid__item info">
           <h3>COMPANY INFO</h3>
-          <p>VERTEX SCAFFOLDING</p>
-          <p>349 Bury Old Road <br> Manchester <br> M25 1PY</p>
+          <p>{{ name }}</p>
+          <p>
+            {{ address[0] }} <br />
+            {{ address[1] }} <br />
+            {{ address[2] }}
+          </p>
         </div>
       </div>
       <div class="copy">
-        <p>&copy; {{ name }} - Company number {{ companyNo }} - <a :href="privacy">Privacy</a></p>
+        <p>
+          &copy; {{ name }} - Company number {{ companyNo }} -
+          <a :href="privacy">Privacy</a>
+        </p>
       </div>
     </div>
   </footer>
@@ -35,7 +51,7 @@
 
 <script>
 export default {
-  name: 'Footer',
+  name: "Footer",
   props: {
     name: {
       type: String,
@@ -53,11 +69,16 @@ export default {
       type: String,
       required: true,
     },
-  }
-}
+    address: {
+      type: Array,
+      required: true,
+    }
+  },
+};
 </script>
 
 <style lang="scss">
-  @import './Footer.scss';
-  @import '../theme/theme.scss';
+// import the theme first for global variables
+@import "../theme/theme.scss";
+@import "./Footer.scss";
 </style>
