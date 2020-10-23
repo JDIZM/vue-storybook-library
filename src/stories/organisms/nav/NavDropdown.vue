e4c9d0e32712d7e7f3ac84ba74fab050f43770bf
JD <james@baked.digital>
2020-10-23 18:33:20 +0100

    vars

--- src/stories/organisms/nav/NavDropdown.vue
+++ src/stories/organisms/nav/NavDropdown.vue
@@ -1,12 +1,12 @@
 <template>
-  <div v-if="showDropdown" class="nav__dropdown">
+  <div v-if="showDropdown" class="nav__dropdown bg--primary">
     <ul>
       <li v-for="item in items" :key="item.name" class="nav__dropdown__item">
         <!-- FIXME links -->
         <a v-if="item.path" :to="item.path" exact-active-class="--active">
           <div class="nav__desktop__list__item">
-            <span v-if="item.icon" class="material-icons">{{ item.icon }}</span>
-            <span>{{ item.name.toUpperCase() }}</span>
+            <span v-if="item.icon" class="material-icons text--white">{{ item.icon }}</span>
+            <span class="text--white">{{ item.name.toUpperCase() }}</span>
           </div>
         </a>
       </li>
