7d55c81307e76d74987db7001654119aaf1b0302
JD <james@baked.digital>
2020-10-23 16:23:11 +0100

    ..

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,21 +10,23 @@ module.exports = {
     "@storybook/preset-scss"
   ],
   // TODO load global sass styles
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
+  // global sass is loaded in preview.js
+  // FIXME vars wont work inside components.
+  // webpackFinal: async (config, { configType }) => {
+  //   // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  //   // You can change the configuration based on that.
+  //   // 'PRODUCTION' is used when building the static version of storybook.
 
-    // Make whatever fine-grained changes you need
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, './src/stories/theme/theme.scss'),
-    });
+  //   // Make whatever fine-grained changes you need
+  //   config.module.rules.push({
+  //     test: /\.scss$/,
+  //     use: ['style-loader', 'css-loader', 'sass-loader'],
+  //     include: path.resolve(__dirname, './src/stories/theme/theme.scss'),
+  //   });
 
-    // Return the altered config
-    return config;
-  }
+  //   // Return the altered config
+  //   return config;
+  // }
 }
 
 // .storybook/main.js
