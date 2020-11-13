dcfe0038fe2331f53a6fdb14ed8f65cb8a470adf
JD <james@baked.digital>
2020-11-13 11:41:34 +0000

    dropdown

---
+++ src/components/molecules/Dropdown.stories.js
@@ -0,0 +1,33 @@
+import Dropdown from './Dropdown.vue'
+
+export default {
+  title: "molecules/Dropdown",
+  component: Dropdown,
+  argTypes: {
+    onClick: { action: "onClick" }
+  }
+};
+
+// create template
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Dropdown },
+  template: '<Dropdown v-bind="$props" @onClick="onClick" />'
+});
+
+//
+export const Primary = Template.bind({});
+Primary.args = {
+  name: "Dropdown",
+  dropdowns: [
+    { title: 'ONE TITLE', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis non, dignissimos asperiores ipsum aliquam expedita porro enim ipsam sequi? Numquam esse incidunt quam nihil nam fugiat dignissimos dolorum reiciendis tempora', isActive: true },
+    { title: 'TWO TITLE', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis non, dignissimos asperiores ipsum aliquam expedita porro enim ipsam sequi? Numquam esse incidunt quam nihil nam fugiat dignissimos dolorum reiciendis tempora', isActive: false }
+  ]
+  // label: "label",
+  // icon: "phone",
+  // placeholder: "Enter some text",
+  // type: "text",
+  // showIcon: false,
+  // showError: false,
+  // value: ''
+};
---
+++ src/components/molecules/Dropdown.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="dropdowns">
+    <div v-for="dropdown in dropdowns" :key="dropdown.title" class="dropdown pb--2 pt--2">
+      <div class="dropdown__trigger" :class="{ 'active': dropdown.isActive }" @click="activate(dropdown)">
+        {{ dropdown.title }}
+      </div>
+      <div class="dropdown__content" :class="{ 'active': dropdown.isActive }">
+        <p>
+          {{ dropdown.content }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dropdown',
+  props: {
+    dropdowns: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      //
+      // dropdowns: [
+      //   { title: 'ONE TITLE', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis non, dignissimos asperiores ipsum aliquam expedita porro enim ipsam sequi? Numquam esse incidunt quam nihil nam fugiat dignissimos dolorum reiciendis tempora', isActive: false },
+      //   { title: 'TWO TITLE', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis non, dignissimos asperiores ipsum aliquam expedita porro enim ipsam sequi? Numquam esse incidunt quam nihil nam fugiat dignissimos dolorum reiciendis tempora', isActive: false }
+      // ]
+    }
+  },
+  methods: {
+    activate (dropdown) {
+      // console.log(dropdown)
+      dropdown.isActive = !dropdown.isActive
+      this.$emit('onClick', dropdown)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './Dropdown.scss';
+.dropdowns {
+  width: 100%;
+}
+</style>
