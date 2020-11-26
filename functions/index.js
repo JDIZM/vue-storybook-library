76acace0ae094a68d2c9a4bf95a44e6d6fd0849c
JD <james@baked.digital>
2020-11-26 17:17:21 +0000

    mailchimp firebase functions! :tada:

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,13 @@
+const dotenv = require('dotenv')
+dotenv.config()
+// const cors = require('cors')({ origin: true })
 const functions = require('firebase-functions');
+const Mailchimp = require('mailchimp-api-v3')
+// const key = process.env.YOUR_API_KEY;
+// FIXME dotenv doesn't seem to be working
+const key = '790042f849269326fe453c31d6d91bc7-us17';
+const mailchimp = new Mailchimp(key);
+// const api = 'https://us17.api.mailchimp.com/3.0/lists';
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +16,46 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+exports.getListId = functions.https.onRequest((request, response) => {
+  // post data to the http function
+  // access the request and response objects
+  // sample list = /lists/19e84c190f
+  const { list } = request.body
+  // get list id
+  mailchimp.get({
+    // path : '/lists/19e84c190f'
+    path: list
+  })
+  .then(result => {
+    // console.log(result)
+    return response.json(result)
+  })
+  .catch(err => {
+    // console.log(error);
+    return response.json(err)
+  })
+});
+// it works!!! woohoo!!!
+
+exports.newSubscriber = functions.https.onRequest((request, response) => {
+  // post data to the http function
+  // access the request and response objects
+  // sample list = /lists/19e84c190f
+  const { email } = request.body
+  // get list id
+  // list = /lists/19e84c190f
+  // path = /members
+  // /lists/19e84c190f/members
+  mailchimp.post('/lists/19e84c190f/members', {
+    email_address : email,
+    status : 'subscribed'
+  })
+  .then(result => {
+    // console.log(result)
+    return response.json(result)
+  })
+  .catch(err => {
+    // console.log(error);
+    return response.json(err)
+  })
+});
