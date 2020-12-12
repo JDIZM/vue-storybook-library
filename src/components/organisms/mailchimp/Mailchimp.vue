f89886755a4134b56e554f4e62d2b99b90f1d6d9
JD <james@baked.digital>
2020-12-12 04:01:20 +0000

    mailchimp updated

--- src/components/organisms/mailchimp/Mailchimp.vue
+++ src/components/organisms/mailchimp/Mailchimp.vue
@@ -2,19 +2,21 @@
   <div class="form">
     <form @submit.prevent="onSubmit">
       <!-- <div :class="icons"></div> -->
+      <!-- TODO test error classes -->
       <FormInput 
         name="email"
         type="email"
-        label="Subscribe to our newsletter?"
+        :label="label"
         placeholder="Enter your email"
         icon="email"
         :showIcon="true"
-        :showError="false"
+        :showError="showError"
+        :showSuccess="showSuccess"
         v-model="email"
         @update="onUpdate"
         required
       />
-      <span v-if="showError" class="text--error">please enter an email address </span>
+      <span v-if="showError" class="text--error">please enter an email address</span>
       <Button
         @onClick="onSubmit"
         type="submit"
@@ -36,28 +38,20 @@ export default {
     Button
   },
   props: {
-    // left: {
-    //   type: Boolean,
-    //   required: false,
-    //   default: () => false
-    // },
-    // showIcon: {
-    //   type: Boolean,
-    //   required: false,
-    //   default: () => false
-    // },
-    // icon: {
-    //   type: String,
-    //   required: true
-    // },
-    // label: {
-    //   type: String,
-    //   required: true
-    // },
-    // placeholder: {
-    //   type: String,
-    //   required: true
-    // }
+    label: {
+      type: String,
+      required: true
+    },
+    showSuccess: {
+      type: Boolean,
+      required: false,
+      default: () => false
+    },
+    showError: {
+      type: Boolean,
+      required: false,
+      default: () => false
+    }
   },
   model: {
     data: "value",
@@ -66,39 +60,49 @@ export default {
   data () {
     return {
       //
-      email: '',
-      showError: false
+      email: ''
     }
   },
   methods: {
     onSubmit () {
-      // TODO validate on submit
+      // validate on submit
       if (this.email === null || this.email === '') {
         this.showError = true
       }
-      this.$emit('onSubmit', this.updateForm)
+      this.$emit('onSubmit', this.email)
       // TODO send to mailchimp firebase function
-      // handle within the component or not?
     },
     onUpdate(value) {
       this.$emit('onUpdate', value)
+    },
+    // FIXME outside or inside the component?
+    async subscribeToList (email) {
+      // https://mailchimp.com/developer/guides/marketing-api-quick-start/#install-the-client-library-for-your-language
+      await this.axios.post('/api/mailchimp/', email, {
+       headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+      })
+      .then((response) => {
+        console.log(response.data)
+      })
     }
   },
   computed: {
     // use computed values to map form data to an object.
-    updateForm () {
-      return {
-          email: this.email,
-      }
-    },
-    // FIXME only works with dynamic classes, doesn't work with props.
+    // updateForm () {
+    //   return {
+    //       email: this.email,
+    //   }
+    // }
+    //  dynamic icons only work with class strings, doesn't work with props.
     // returns an object with props, returns the string value with classes
-    icons () {
-      return {
-          'email': !this.showError,
-          'error': this.showError
-      }
-    }
+    // icons () {
+    //   return {
+    //       'email': !this.showError,
+    //       'error': this.showError
+    //   }
+    // }
   }
 }
 </script>
