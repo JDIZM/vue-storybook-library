a8cb38dbe6a58b19e18172cb155c3a46e9c4ad1e
JD <james@baked.digital>
2020-12-14 17:36:33 +0000

    custom theme and logo

---
+++ .storybook/theme.js
@@ -0,0 +1,11 @@
+// https://storybook.js.org/docs/vue/configure/theming
+// .storybook/theme.js
+
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Vue Storybook Library',
+  brandUrl: 'https://example.com',
+  brandImage: './logo.png',
+});
