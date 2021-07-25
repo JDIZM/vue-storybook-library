66c26a72b150aa93b8c2f2a77182b92d57c57a7f
JDIZM <4444684+JDIZM@users.noreply.github.com>
2021-07-25 21:14:42 +0100

    base responsive image component

---
+++ stories/atoms/BaseImage.stories.js
@@ -0,0 +1,28 @@
+import BaseImage from './BaseImage.vue'
+
+export default {
+  title: 'atoms/Responsive Image',
+  components: { BaseImage },
+};
+
+// create multiple src images to match the srcset sizes
+// just using one size for example
+import src from '../assets/lionel-richie.jpg'
+const srcset = `${src} 300w, ${src} 600w, ${src} 900w`
+const alt = "lionel richie"
+const sizes = "(max-width: 500px) 300px, (max-width: 800px) 600px, 100vw"
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { BaseImage },
+  template: '<BaseImage v-bind="$props" :src="src" :srcset="srcset" :alt="alt" :style="style" />',
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  alt,
+  src,
+  srcset,
+  sizes,
+  style: 'width: 500px; height: 500px'
+}
---
+++ stories/atoms/BaseImage.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="image">
+    <picture >
+      <source
+        :src="src"
+        :alt="alt"
+        :srcSet="srcset"
+        :sizes="sizes"
+        type="image/webp"
+      />
+      <img :src="src" :alt="alt" :srcSet="srcset" :sizes="sizes" draggable="false" />
+    </picture>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    srcset: {
+      type: String,
+      required: true,
+    },
+    alt: {
+      type: String,
+      required: true,
+    },
+    sizes: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.image {
+  height: 100%;
+  width: 100%;
+  margin: auto;
+}
+img {
+  width: 100%;
+  height: 100%;
+}
+</style>
