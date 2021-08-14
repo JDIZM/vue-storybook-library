469e0de6f7d8ad05bb438700ec51dd256f3571f4
JDIZM <4444684+JDIZM@users.noreply.github.com>
2021-08-14 22:28:41 +0100

    click.prevent

--- stories/atoms/Button.vue
+++ stories/atoms/Button.vue
@@ -1,33 +1,38 @@
 <template>
-  <button type="button" :class="classes" @click="onClick" :style="style">
+  <button
+    type="button"
+    :class="classes"
+    :style="style"
+    @click.prevent="onClick"
+  >
     {{ label }}
   </button>
 </template>
 
 <script>
 export default {
-  name: 'Button',
+  name: "Button",
   props: {
     //
     primary: {
       type: Boolean,
       required: false,
-      default: () => false
+      default: () => false,
     },
     outline: {
       type: Boolean,
       required: false,
-      default: () => false
+      default: () => false,
     },
     disabled: {
       type: Boolean,
       required: false,
-      default: () => false
+      default: () => false,
     },
     destruct: {
       type: Boolean,
       required: false,
-      default: () => false
+      default: () => false,
     },
     label: {
       type: String,
@@ -39,48 +44,48 @@ export default {
     left: {
       type: Boolean,
       required: false,
-      default: () => false
+      default: () => false,
     },
   },
-  data () {
+  data() {
     return {
       //
       // styles: ['btn', 'btn--primary']
-    }
-  },
-  methods: {
-    onClick () {
-     this.$emit('onClick');
-    }
+    };
   },
   computed: {
     // class binding method 1
     isPrimary() {
-      if(this.primary){
-        return ['btn', 'btn--primary']
-      } else return ['btn', 'btn--secondary']
+      if (this.primary) {
+        return ["btn", "btn--primary"];
+      } else return ["btn", "btn--secondary"];
     },
     // class binding method 2
     classes() {
       return {
-        'btn': true,
-        'btn--primary': this.primary,
-        'btn--outline': this.outline,
-        'btn--disabled bg--disabled text--dark': this.disabled,
-        'btn--destruct bg--destruct text--light': this.destruct,
-        'btn--left': this.left
-      }
+        btn: true,
+        "btn--primary": this.primary,
+        "btn--outline": this.outline,
+        "btn--disabled bg--disabled text--dark": this.disabled,
+        "btn--destruct bg--destruct text--light": this.destruct,
+        "btn--left": this.left,
+      };
     },
     // inline style binding
     style() {
       return {
         backgroundColor: this.backgroundColor,
-      }
-    }
-  }
-}
+      };
+    },
+  },
+  methods: {
+    onClick() {
+      this.$emit("onClick");
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@import './Button.scss';
+@import "./Button.scss";
 </style>
