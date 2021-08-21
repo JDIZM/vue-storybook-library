ab652551a158b5c6d3890c0ae149f1728531c918
JDIZM <4444684+JDIZM@users.noreply.github.com>
2021-08-21 18:11:59 +0100

    component exports

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,45 @@
 </template>
 
 <script>
-export default {};
+// test imports
+import { VButton } from "./components";
+import { VImg } from "./components";
+import { VDropdown } from "./components";
+import { VFormInput } from "./components";
+import { VModal } from "./components";
+import { VToast } from "./components";
+import { VSlider } from "./components";
+import { VContactForm } from "./components";
+import { VEmailSubscribe } from "./components";
+import { VNav } from "./components";
+import { VFooter } from "./components";
+import { VNotFound } from "./components";
+export default {
+  created() {
+    // test imported components
+    console.log(VButton);
+    console.log(VImg);
+    console.log(VDropdown);
+    console.log(VFormInput);
+    console.log(VModal);
+    console.log(VToast);
+    console.log(VSlider);
+    console.log(VContactForm);
+    console.log(VEmailSubscribe);
+    console.log(VNav);
+    console.log(VFooter);
+    console.log(VNotFound);
+  },
+};
 </script>
 
-<style>
-
+<style lang="scss">
+// access scss vars
+@import "../theme/_vars.scss";
+#app {
+  // use map-get for scss vars
+  background-color: map-get($colours, secondary);
+  // use base css vars
+  background-color: var(--primary);
+}
 </style>
