2a2774b36a90a0489a25b563ea498efe6e75017e
JDIZM <4444684+JDIZM@users.noreply.github.com>
2021-11-23 19:47:51 +0000

    chore: rm comments

--- src/entry.js
+++ src/entry.js
@@ -1,7 +1,5 @@
-// Import vue components
-// import * as components from "@/components/index";
+// library entry point
 
-// console.log(components);
 // To allow individual component use, export components
 // each can be registered via Vue.component()
 export * from "@/components";
