f53146e231695131e251bc41df853e194df01093
JDIZM <4444684+JDIZM@users.noreply.github.com>
2021-12-19 14:00:05 +0000

    feat: export icons

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,21 @@ import "../../theme/main.scss";
 //
 export { default as VButton } from "../../stories/atoms/button/Button.vue";
 export { default as VImg } from "../../stories/atoms/image/BaseImage.vue";
+export { default as IconAlarm } from "../../stories/atoms/icons/Alarm.vue";
+export { default as IconClose } from "../../stories/atoms/icons/Close.vue";
+export { default as IconDone } from "../../stories/atoms/icons/Done.vue";
+export { default as IconEmail } from "../../stories/atoms/icons/Email.vue";
+export { default as IconGithub } from "../../stories/atoms/icons/Github.vue";
+export { default as IconInfo } from "../../stories/atoms/icons/Info.vue";
+export { default as IconInstagram } from "../../stories/atoms/icons/Instagram.vue";
+export { default as IconLinkedIn } from "../../stories/atoms/icons/LinkedIn.vue";
+export { default as IconList } from "../../stories/atoms/icons/List.vue";
+export { default as IconMenu } from "../../stories/atoms/icons/Menu.vue";
+export { default as IconMessage } from "../../stories/atoms/icons/Message.vue";
+export { default as IconPerson } from "../../stories/atoms/icons/Person.vue";
+export { default as IconPhone } from "../../stories/atoms/icons/Phone.vue";
+export { default as IconTwitter } from "../../stories/atoms/icons/Twitter.vue";
+export { default as IconWarning } from "../../stories/atoms/icons/Warning.vue";
 
 //
 // molecules
