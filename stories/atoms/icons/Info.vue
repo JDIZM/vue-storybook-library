35ea7bd7f1b86e8aad86b40c8b65bc2bc56d9def
JDIZM <4444684+JDIZM@users.noreply.github.com>
2021-12-19 13:54:17 +0000

    feat: svg icons

---
+++ stories/atoms/icons/Info.vue
@@ -0,0 +1,41 @@
+<template>
+  <svg
+    aria-hidden="true"
+    focusable="false"
+    role="img"
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 0 24 24"
+    width="24px"
+    fill="currentColor"
+    class="icon"
+    :class="classes"
+  >
+    <path d="M0 0h24v24H0V0z" fill="none" />
+    <path
+      d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    small: Boolean,
+    medium: Boolean,
+    large: Boolean,
+  },
+  computed: {
+    classes() {
+      return {
+        "icon--md": this.medium,
+        "icon--lg": this.large,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@use "./Icons.scss";
+</style>
---
+++ stories/atoms/icons/icons.stories.js
@@ -0,0 +1,73 @@
+import Alarm from "./Alarm.vue";
+import Close from "./Close.vue";
+import Done from "./Done.vue";
+import Email from "./Email.vue";
+import Info from "./Info.vue";
+import List from "./List.vue";
+import Menu from "./Menu.vue";
+import Message from "./Message.vue";
+import Person from "./Person.vue";
+import Phone from "./Phone.vue";
+import Warning from "./Warning.vue";
+import Twitter from "./Twitter.vue";
+import LinkedIn from "./LinkedIn.vue";
+import Instagram from "./Instagram.vue";
+import Github from "./Github.vue";
+
+export default {
+  title: "atoms/Icons",
+  argTypes: {
+    color: { control: "color" },
+  },
+};
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: {
+    Alarm,
+    Close,
+    Done,
+    Email,
+    Info,
+    List,
+    Menu,
+    Message,
+    Person,
+    Phone,
+    Warning,
+    Twitter,
+    LinkedIn,
+    Instagram,
+    Github,
+  },
+  template: `
+      <div>
+        <alarm v-bind="$props" :color="color" />
+        <close v-bind="$props" :color="color" />
+        <done v-bind="$props" :color="color" />
+        <email v-bind="$props" :color="color" />
+        <info v-bind="$props" :color="color" />
+        <list v-bind="$props" :color="color" />
+        <menu v-bind="$props" :color="color" />
+        <message v-bind="$props" :color="color" />
+        <person v-bind="$props" :color="color" />
+        <phone v-bind="$props" :color="color" />
+        <warning v-bind="$props" :color="color" />
+        <twitter v-bind="$props" :color="color" />
+        <linked-in v-bind="$props" :color="color" />
+        <instagram v-bind="$props" :color="color" />
+        <github v-bind="$props" :color="color" />
+      </div>
+    `,
+});
+
+export const Small = Template.bind({});
+
+export const Medium = Template.bind({});
+Medium.args = {
+  medium: true,
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  large: true,
+};
