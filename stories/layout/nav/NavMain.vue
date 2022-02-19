33935e567eb37bca9f33d7ceed4bd38c983e4db5
JDIZM <4444684+JDIZM@users.noreply.github.com>
2022-02-19 21:24:03 +0000

    refactor: remove span from nav toggle

--- stories/layout/nav/NavMain.vue
+++ stories/layout/nav/NavMain.vue
@@ -87,8 +87,8 @@
       tabindex="0"
       @click="onShowDrawer"
     >
-      <span v-if="!showDrawer" class="icons"><Menu medium /></span>
-      <span v-else class="icons"><Close medium /></span>
+      <Menu v-if="!showDrawer" medium class="icons" />
+      <Close v-else medium class="icons" />
     </div>
     <NavDrawer
       :links="links"
