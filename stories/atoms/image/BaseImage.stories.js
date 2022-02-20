28b1b6017cd85b66c0f5ed97687a384186a3a649
JDIZM <4444684+JDIZM@users.noreply.github.com>
2022-02-20 15:36:55 +0000

    refactor: responsive image with multiple sizes

--- stories/atoms/image/BaseImage.stories.js
+++ stories/atoms/image/BaseImage.stories.js
@@ -5,17 +5,26 @@ export default {
   components: { BaseImage },
 };
 
-// create multiple src images to match the srcset sizes
-// just using one size for example
+// https://developer.mozilla.org/en-US/docs/Learn/HTML/Multimedia_and_embedding/Responsive_images#how_do_you_create_responsive_images
+
+// original image 1200x1200
 import src from "../../assets/lionel-richie.jpg";
-const srcset = `${src} 300w, ${src} 600w, ${src} 900w`;
+
+// create images for specific breakpoints
+import one from "../../assets/progressive-300.jpg";
+import two from "../../assets/progressive-600.jpg";
+import three from "../../assets/progressive-900.jpg";
+
+// create custom srcset and sizes
+const srcset = `${one} 300w, ${two} 600w, ${three} 900w, ${src}`;
 const alt = "lionel richie";
-const sizes = "(max-width: 500px) 300px, (max-width: 800px) 600px, 100vw";
+const sizes =
+  "(max-width: 500px) 300px, (max-width: 800px) 600px, (max-width: 1100px) 900px, 100vw";
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { BaseImage },
-  template: '<BaseImage v-bind="$props" style="height: 400px; width: 400px;"/>',
+  template: '<BaseImage v-bind="$props"/>',
 });
 
 export const Primary = Template.bind({});
