afa4a1443549b0e9006efd4ac7b1502ea672c802
JDIZM <4444684+JDIZM@users.noreply.github.com>
2022-03-27 13:36:24 +0100

    feat: contact form additional props

--- stories/organisms/contact-form/ContactForm.vue
+++ stories/organisms/contact-form/ContactForm.vue
@@ -1,7 +1,15 @@
 <template>
   <form class="form" @submit.prevent>
     <slot />
-    <Button type="submit" left label="SUBMIT" primary @click="onSubmit" />
+    <Button
+      v-show="!hideSubmit"
+      type="submit"
+      left
+      :label="label"
+      primary
+      :disabled="disableSubmit"
+      @click="onSubmit"
+    />
   </form>
 </template>
 
@@ -11,6 +19,15 @@ import Button from "../../atoms/button/Button.vue";
 export default {
   components: {
     Button,
+  },
+  props: {
+    disableSubmit: Boolean,
+    hideSubmit: Boolean,
+    label: {
+      type: String,
+      required: false,
+      default: "SUBMIT",
+    },
   },
   methods: {
     onSubmit() {
